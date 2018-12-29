f1185279227f4ea1162499369e16ba84109aef7c
gleicher (home) <gleicher@cs.wisc.edu>
2018-12-29 13:51:20 -0600

    clean up one in order to type check correctly

--- one.js
+++ one.js
@@ -1,11 +1,15 @@
 // note that we don't do a global "use strict" because this can create a problem
 // if we include another file
 // "use strict";
+// @ts-check
 
-// Mike's first attempt at using an ES6 module
-'esversion:6';
+/* disable the warning about using bracket rather than dot
+ * even though dot is better 
+ * https://stackoverflow.com/questions/13192466/how-to-suppress-variable-is-better-written-in-dot-notation
+ */
+/* jshint -W069, esversion:6 */
 
-console.log("Before the mymodule import!")
+console.log("Before the mymodule import!");
 
 // test that import works
 import * as mymodule from "./mymodule.js";
@@ -18,7 +22,7 @@ console.log("File One is being loaded");
 //
 // also note that we take advantage of function hoisting
 // (we use the function declaration even before it is used)
-window.onload = startProgram();
+window.onload = startProgram;
 
 // a function that takes care of the action - this is what gets run
 // when the program starts - see window.onload above
@@ -66,45 +70,45 @@ function startProgram() {
         let b2s = document.getElementById("button2span");
         b2s.innerText = "The button was pushed!";
         b2s.style["background"] = "#CEC";
-    }
+    };
 
     // Button 3
     // another way to do something with the button
     let b3 = document.getElementById("button3");
-    let b3s = document.getElementById("button3span")
+    let b3s = document.getElementById("button3span");
     // attach something to the mouse down and up events
     b3.onmousedown = function() {
         // because of closure/scope, we can use b3s from outside
         // rather than trying to do the getElementByID inside the function
         b3s.innerText = "Button is being pressed!";
         b3s.style["background"] = "#FCC";
-    }
+    };
     // Note That this is fragile! We only get the up event if the mouse stays
     // inside the button
     b3.onmouseup = function() {
-        b3s.innerText = "Button was pressed!"
+        b3s.innerText = "Button was pressed!";
         b3s.innerText += "If you want to break this, move the mouse outside the button before releasing the button.";
         b3s.style["background"] = "#CFC";
-    }
+    };
 
     // Button 4 - Button 3 that's harder to break
     let b4 = document.getElementById("button4");
-    let b4s = document.getElementById("button4span")
+    let b4s = document.getElementById("button4span");
     // attach something to the mouse down and up events
     b4.onmousedown = function() {
         // because of closure/scope, we can use b3s from outside
         // rather than trying to do the getElementByID inside the function
         b4s.innerText = "Button is being pressed!";
         b4s.style["background"] = "#FCC";
-    }
+    };
     // Note That this is fragile! We only get the up event if the mouse stays
     // inside the button. But we only get the up event if we had gotten the
     // down event
     b4.onclick = function() {
-        b4s.innerText = "Button was pressed!"
+        b4s.innerText = "Button was pressed!";
         b4s.innerText += "This breaks differently.";
         b4s.style["background"] = "#CFC";
-    }
+    };
 
     // Button 5 - compare onclick and onmouseup
     let b5mu = document.getElementById("button5mu");
@@ -112,14 +116,16 @@ function startProgram() {
     let b5s = document.getElementById("b5s");
    
     // use the same function for both buttons
-    // we'll take advantage of the this being "correct"
-    function b5down() {
-        let mstr = this.innerText + " was pressed and not released yet";
+    // normally, I'd take advantage of the this being "correct"
+    // but that's unsafe (who knows who will call these functions!)
+    // (it's still officially un-typesafe since currentTarget might not have an innerText)
+    function b5down(event) {
+        let mstr = event.currentTarget.innerText + " was pressed and not released yet";
         b5s.innerText = mstr;
         b5s.style["background"] = "#CEC";
     }
-    function b5up() {
-        let mstr = this.innerText + " was released";
+    function b5up(event) {
+        let mstr = event.currentTarget.innerTextinnerText + " was released";
         b5s.innerText = mstr;
         b5s.style["background"] = "#CCE";
     }
@@ -138,7 +144,7 @@ function startProgram() {
     b6.onmousedown = function() {
         b6state = 1;
         b6s.innerText = "Button 6 down";
-    }
+    };
     b6.onmouseup = function() {
         if (b6state) {
             b6s.innerText = "Button 6 released (after press)";
@@ -146,28 +152,32 @@ function startProgram() {
             b6s.innerText = "Button 6 released (without press)";
         }
         b6state = 0;
-    }
+    };
     b6.onmouseleave = function() {
         b6state = 0;
         b6s.innerText = "Button 6 left before release";
-    }
+    };
 
     // here, we make a button programmatically
-    let span7 = document.getElementById("span7")
+    let span7 = document.getElementById("span7");
     let b7 = document.createElement("button"); 
-    b7.innerText = "Button 7"
+    b7.innerText = "Button 7";
     span7.appendChild(b7);
     b7.onclick = function() {
         let b7s = document.getElementById("b7s");
-        b7s.innerText = "Button 7 was pressed."
-    }
+        b7s.innerText = "Button 7 was pressed.";
+    };
 
     /////////////////////////////////////////////////////////
     // Slider Demos
 
     // the slider demo
     // make a slider and try out some different events
-    let slider2 = document.getElementById("sl2");
+    /** @type {HTMLInputElement} */
+    let slider2 = (/** @type {HTMLInputElement} */document.getElementById("sl2"));
+    if (! (slider2 instanceof HTMLInputElement)) {
+        throw("Expected Slider - but got something else!");
+    }
     let sl2a = document.getElementById("sl2a");
     let sl2b = document.getElementById("sl2b");
     let sl2c = document.getElementById("sl2c");
@@ -175,38 +185,37 @@ function startProgram() {
 
     slider2.onchange = function() {
         sl2a.innerText = "Slider Change to " + slider2.value;
-    }
+    };
     slider2.onclick = function() {
         sl2b.innerText = "Slider Click at " + slider2.value;
-    }
+    };
     slider2.onmousedown = function() {
         sl2c.innerText = "Slider Mousedown at " + slider2.value;
-    }
+    };
     slider2.oninput = function() {
         sl2d.innerText = "Slider OnInput at " + slider2.value;
-    }
+    };
 
 
     // the second slider demo - #1 tied to number 2 and 3
-    let slider3 = document.getElementById("sl3");
-    let slider3a = document.getElementById("sl3a");
-    let slider3b = document.getElementById("sl3b");
-    let sp3 = document.getElementById("sl3s")
+    /** @type{HTMLInputElement} */ let slider3 =  (/** @type{HTMLInputElement} */ document.getElementById("sl3"));
+    /** @type{HTMLInputElement} */ let slider3a = (/** @type{HTMLInputElement} */ document.getElementById("sl3a"));
+    /** @type{HTMLInputElement} */ let slider3b = (/** @type{HTMLInputElement} */ document.getElementById("sl3b"));
+    let sp3 = document.getElementById("sl3s");
 
     slider3.oninput = function() {
         slider3a.value = slider3.value;
-    }
+    };
     slider3.onchange = function() {
         slider3b.value = slider3.value;
-    }
+    };
     slider3.onmousedown = function() {
         sp3.innerText = "Slider 3 being dragged";
-    }
+    };
     // use chaining to set both of these
     slider3a.onmousedown = slider3b.onmousedown = function() {
         sp3.innerText = "Slider not connected to anything";
-    }
-
+    };
 
     /////////////////////////////////////////////////////////
     // requestAnimationFrame examples
@@ -214,10 +223,10 @@ function startProgram() {
     // use request animation frame to do something 16ms
     // in the future - this kindof one off usage is a little
     // weird
-    let timestamp = performance.now()
+    let timestamp = performance.now();
     window.requestAnimationFrame(function() {
         let span = document.getElementById("rs1");
-        rs1.innerText = "Hello from "+(performance.now()-timestamp)+" in the future";
+        span.innerText = "Hello from "+(performance.now()-timestamp)+"ms in the future";
     });
 
     let rs2 = document.getElementById("rs2");
@@ -235,12 +244,12 @@ function startProgram() {
             // function (thanks to closure!)
             rs2.innerText += " Hello from Second "+(now-ts1)+" ms after First.";
         });
-    })
+    });
 
     // a more typical usage of request animation frame
     // a function schedules itself to be called in order to
     // make a loop
-    let slr1 = document.getElementById("slr1");
+    /** @type{HTMLInputElement} */ let slr1 = (/** @type{HTMLInputElement} */ document.getElementById("slr1"));
     function advanceSLR1() {
         // add 1, roll over to zero if we hit the max
         // note that the value of the slider is a string,
@@ -248,7 +257,7 @@ function startProgram() {
         // The more obvious 1+"1" = "11" - thanks to JavaScripts
         // aggressive coercion rules
         let newValue = (Number(slr1.value)+1) % 100;
-        slr1.value = newValue;
+        slr1.value = newValue.toString();
         // ask for this to be called again 16ms in the future
         window.requestAnimationFrame(advanceSLR1);
     }
@@ -259,8 +268,8 @@ function startProgram() {
     // this example is really similar to the first - notice how
     // both sliders can work, even though neither generates
     // any events. everything happens by "polling."
-    let slr2 = document.getElementById("slr2");
-    let slr2b = document.getElementById("slr2b");
+    /** @type{HTMLInputElement} */ let slr2 = (/** @type{HTMLInputElement} */ document.getElementById("slr2"));
+    /** @type{HTMLInputElement} */ let slr2b = (/** @type{HTMLInputElement} */ document.getElementById("slr2b"));
     function advanceSLR2() {
         // add speed, roll over to zero if we hit the max
         // note that the value of the slider is a string,
@@ -270,7 +279,7 @@ function startProgram() {
         let speed = Number(slr2.value);
         let newValue = (Number(slr2b.value)+speed) % 100;
         if (newValue<0) newValue=100;
-        slr2b.value = newValue;
+        slr2b.value = newValue.toString();
         // ask for this to be called again 16ms in the future
         window.requestAnimationFrame(advanceSLR2);
     }
